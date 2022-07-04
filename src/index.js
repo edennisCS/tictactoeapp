831469d8a89996952c741fe7c65ff1ef3e4a56a3
candidate-139715 <candidate-139715@candidate.hackajob.co>
2022-07-04 14:51:15 +0000

    auto-commit-agent

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 constructor(props) {
-  super(props);
-  this.state = {
-    value: null,
-  };
-}
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
 class Square extends React.Component {
-
+  render() {
+    return (
+      <button className="square" onClick={() => { alert('click'); }}>
+        {this.props.value}
+      </button>
+    );
+  }
 }
 
 class Board extends React.Component {
