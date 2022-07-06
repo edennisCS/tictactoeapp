c2b088b7853c13904aab1a6590b874c479745fc5
candidate-139715 <candidate-139715@candidate.hackajob.co>
2022-07-06 08:26:25 +0000

    auto-commit-agent

--- src/index.js
+++ src/index.js
@@ -9,6 +9,17 @@ function Square(props) {
     </button>
   );
 
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
+  }
+}
 
 class Board extends React.Component {
     constructor(props) {
