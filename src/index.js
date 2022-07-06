40505617ca848b678dbfce31b1e7f4cc89060bf3
candidate-139715 <candidate-139715@candidate.hackajob.co>
2022-07-06 08:10:02 +0000

    auto-commit-agent

--- src/index.js
+++ src/index.js
@@ -31,6 +31,11 @@ class Board extends React.Component {
   
   
   }
+    handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
   renderSquare(i) {
     return (
       <Square
