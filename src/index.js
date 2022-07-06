8e1444566e782f702ebd143f0e98b56bd1b17001
candidate-139715 <candidate-139715@candidate.hackajob.co>
2022-07-06 08:49:20 +0000

    auto-commit-agent

--- src/index.js
+++ src/index.js
@@ -30,14 +30,7 @@ class Board extends React.Component {
       />
     );
   }
-    render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
+
 
     return (
       <div>
