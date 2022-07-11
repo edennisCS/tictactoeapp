47dcf310a3bcf6499ac5cd79c1ef32a671b804c2
candidate-139715 <candidate-139715@candidate.hackajob.co>
2022-07-11 14:43:33 +0000

    auto-commit-agent

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-unction Square(props) {
+function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
       {props.value}
