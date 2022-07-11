a0ae8d603406263ab90c51f2dd1b731494cc8d6e
candidate-139715 <candidate-139715@candidate.hackajob.co>
2022-07-11 14:33:25 +0000

    auto-commit-agent

--- src/index.js
+++ src/index.js
@@ -66,19 +66,7 @@ class Game extends React.Component {
       xIsNext: true,
     };
   }
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
+  
 }
 
 function calculateWinner(squares) {
