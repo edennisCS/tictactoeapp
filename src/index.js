f4af76c4c496c778cc7b944207665e7eeda89733
candidate-139715 <candidate-139715@candidate.hackajob.co>
2022-07-11 14:39:09 +0000

    auto-commit-agent

--- src/index.js
+++ src/index.js
@@ -11,6 +11,18 @@ function Square(props) {
 }
 
 class Board extends React.Component {
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   renderSquare(i) {
     return (
       <Square
@@ -19,10 +31,19 @@ class Board extends React.Component {
       />
     );
   }
- 
+
   render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div>
+        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -42,7 +63,6 @@ class Board extends React.Component {
     );
   }
 }
-
 class Game extends React.Component {
     constructor(props) {
     super(props);
